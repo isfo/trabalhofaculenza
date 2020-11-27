89ab71b42e163bc65e2364c131badeebca45c815
Itallo Silva Fideli Oliveira <itallo.fidelis@escolaweb.com.br>
2020-11-27 17:00:20 -0300

    start

---
+++ testemd5/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace testemd5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(16, 44);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(248, 20);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(16, 83);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(248, 20);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(145, 109);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(119, 23);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Cadastrar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLogin);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnSalvar);
+            this.groupBox1.Controls.Add(this.btnLogar);
+            this.groupBox1.Controls.Add(this.txtSenha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 179);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operação";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(324, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(464, 398);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Location = new System.Drawing.Point(16, 109);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(123, 23);
+            this.btnLogar.TabIndex = 7;
+            this.btnLogar.Text = "Logar";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(16, 138);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(248, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Resetar BD";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(324, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(464, 23);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Atualizar Grid";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLogar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ testemd5/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testemd5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AtualizarGrid();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            AtualizarGrid();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            CadastrarUsuario(txtLogin.Text, txtSenha.Text);
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            ConsultaUsuario(txtLogin.Text, txtSenha.Text);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ExecuteNonQuery("delete from Usuario");
+            AtualizarGrid();
+        }
+
+        public SqlParameter[] GerarUsuarioParams(string login, string senha)
+        {
+            senha = Encriptar(senha);
+
+            return new SqlParameter[]
+            {
+                new SqlParameter("@login",login),
+                new SqlParameter("@senha",senha)
+            };
+        }
+
+        public void CadastrarUsuario(string login, string senha)
+        {
+            if (!CheckUsuario(login))
+                if (ExecuteNonQuery($@"insert into Usuario (login,senha) values (@login, @senha); ", GerarUsuarioParams(login, senha)) > 0)
+                {
+                    MessageBox.Show("Usuário cadastrado com sucesso!");
+                    AtualizarGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Ooops! Não foi possível cadastrar o usuário, tente mais tarde!");
+                }
+            else
+                MessageBox.Show("Ooops! Usuário já cadastrado!");
+        }
+
+        public void ConsultaUsuario(string login, string senha)
+        {
+            DataTable dt = ExecuteSelect($@"select * from Usuario where login = @login and senha = @senha; ", GerarUsuarioParams(login, senha));
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login efetuar com sucesso!");
+            }
+        }
+
+        public bool CheckUsuario(string login)
+        {
+            return Convert.ToInt32(ExecuteScalar(@"select count(1) from Usuario where login = @login", new SqlParameter[] { new SqlParameter("@login", login) })) > 0;
+        }
+
+        public void AtualizarGrid()
+        {
+            dataGridView1.DataSource = ExecuteSelect($@"select * from Usuario");
+
+        }
+
+        public string Encriptar(string senha)
+        {
+            MD5 md5Hash = MD5.Create();
+
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+
+        public SqlConnection CriarConexao()
+        {
+            var temp = Directory.GetCurrentDirectory();
+            SqlConnection coon = new SqlConnection($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+
+
+
+            return coon;
+        }
+
+
+        public int ExecuteNonQuery(string query, SqlParameter[] parametros = null)
+        {
+            using (var conn = CriarConexao())
+            {
+                try
+                {
+                    SqlCommand command = conn.CreateCommand();
+                    conn.Open();
+                    command.CommandText = query;
+                    if (parametros != null)
+                        command.Parameters.AddRange(parametros);
+                    return command.ExecuteNonQuery();
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public object ExecuteScalar(string query, SqlParameter[] parametros)
+        {
+            using (var conn = CriarConexao())
+            {
+                try
+                {
+                    SqlCommand command = conn.CreateCommand();
+                    conn.Open();
+                    command.CommandText = query;
+                    if (parametros != null)
+                        command.Parameters.AddRange(parametros);
+                    return command.ExecuteScalar();
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public DataTable ExecuteSelect(string query, SqlParameter[] parametros = null)
+        {
+            using (var conn = CriarConexao())
+            {
+                try
+                {
+                    SqlCommand command = conn.CreateCommand();
+                    conn.Open();
+                    command.CommandText = query;
+                    if (parametros != null)
+                        command.Parameters.AddRange(parametros);
+
+                    var result = command.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(result);
+                    return dt;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        
+    }
+}
